8b336119514d46c42ddd4895e702f617d2a15e64
Hannes Polte <hannes.polte@gmx.de>
2022-09-29 13:01:34 +0200

    fail the linting

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# Simple Hello World flask app
-
-# create linting error
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,7 +6,3 @@ def hello():
     return "Hello World!"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-    sdfksdfkusdf
-    ksdkfjbsdf
-
-ouwerohwe
